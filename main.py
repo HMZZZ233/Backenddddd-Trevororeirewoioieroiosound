45786ccfdb11ba884f8c52918d886ddb3ee577f1
HMZZZ233 <hamzahwisnudzkay@gmail.com>
2025-08-09 11:54:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
+
 app = FastAPI()
-@app.get("/proxy-audio")
-def proxy_audio(url: str):
-    headers = {
-        "User-Agent": "Mozilla/5.0"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # atau whitelist domain kamu
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+BASE_URL = "https://api.siputzx.my.id/api/ai/gpt3"
+PROMPT = "kamu adalah ai yg nama nya trevor dan developer mu Hamzah W.D. serta kamu ai yang sangat Kasar namun ramah,toxic,suka Ngejokes ga banyak omong dan pintar matematika,bahasa inggris,literasi, bahasa indonesia,biologi,fisika dan ilmu sosial pintar bahasa jepang,inggris dan kamu berbasis chatgpt"  # isi lengkap sesuai mau
+
+@app.get("/chat")
+def chat(message: str):
+    params = {
+        "prompt": PROMPT,
+        "content": message
     }
-    response = requests.get(url, headers=headers, stream=True)
-    return StreamingResponse(response.raw, media_type="audio/wav")
+    r = requests.get(BASE_URL, params=params)
+    return r.json()
